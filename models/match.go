1450ab7b5aeff317a2a0855c0937abce8ff85460
Thomas LACAZE <contact@thomaslacaze.fr>
2021-05-07 22:04:51 +0200

    Add date to match struct

--- models/match.go
+++ models/match.go
@@ -2,6 +2,7 @@ package models
 
 type Match struct {
 	ID       uint          `json:"ID" `
+	Date     uint64        `json:"date" `
 	Name     string        `json:"name" `
 	Location MatchLocation `gorm:"foreignKey:MatchID" json:"location" `
 	Team1    []Player1     `gorm:"foreignKey:MatchID" json:"team1" `
